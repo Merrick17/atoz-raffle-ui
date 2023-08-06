651e24281d66d66f1d95cd86ecefb5d4dfcc108f
safwen ben fredj <safwen.benfredj@gmail.com>
2023-08-07 01:20:30 +0200

    fix

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,5 +1,5 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const programId: PublicKey = new PublicKey(process.env.NEXT_PUBLIC_PROGRAM_ID || "BXpLxLCwAwqP9d273RH9n7GNzsyafrt5wMp658Qg2hcv");
-export const authorityAddress: PublicKey = new PublicKey(process.env.NEXT_PUBLIC_AUTHORITY_ADDRESS || "SoULkUw8WRdZKNT1FhMtCUDggDXCagHtdEFQAmGuM7Z   ");
+export const authorityAddress: PublicKey = new PublicKey(process.env.NEXT_PUBLIC_AUTHORITY_ADDRESS || "SoULkUw8WRdZKNT1FhMtCUDggDXCagHtdEFQAmGuM7Z");
 export const rewardMint: PublicKey = new PublicKey(process.env.NEXT_PUBLIC_REWARD_MINT || "F6weWmuc1vwdL4u38Ro9jKXHEMjP9BoNdWMF5o5TvtJf");
